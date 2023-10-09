73bff8599bf06496d6cf816c84591b4f73507862
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-10-08 18:40:50 -0700

    fix: make preludes public in hir-def

--- crates/hir-def/src/nameres.rs
+++ crates/hir-def/src/nameres.rs
@@ -435,11 +435,11 @@ impl DefMap {
         self.block.map(|block| block.block)
     }
 
-    pub(crate) fn prelude(&self) -> Option<(ModuleId, Option<UseId>)> {
+    pub fn prelude(&self) -> Option<(ModuleId, Option<UseId>)> {
         self.prelude
     }
 
-    pub(crate) fn extern_prelude(
+    pub fn extern_prelude(
         &self,
     ) -> impl Iterator<Item = (&Name, (CrateRootModuleId, Option<ExternCrateId>))> + '_ {
         self.data.extern_prelude.iter().map(|(name, &def)| (name, def))
